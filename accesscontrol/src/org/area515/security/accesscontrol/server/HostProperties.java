bfe6e39567b89a0df5b6673d43ac8e753195c0ba
ergobot <stobryan@gmail.com>
2014-11-22 23:28:53 -0600

    Create acl if it doesn't exist.  Don't read from property file... sounds dangerous

--- accesscontrol/src/org/area515/security/accesscontrol/server/HostProperties.java
+++ accesscontrol/src/org/area515/security/accesscontrol/server/HostProperties.java
@@ -4,9 +4,16 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Properties;
 
+import org.apache.commons.io.FileUtils;
+
 
 public class HostProperties {
 
+	/*
+	 * TODO: Need to clean up. 
+	 * 
+	 */
+	
 	private static HostProperties m_instance = null;
 	public static HostProperties Instance() throws IOException {
 		if (m_instance == null) {
@@ -28,22 +35,33 @@ public class HostProperties {
 	}
 	
 	
+	/*
+	 * init()
+	 * Check if "/home/user/rfid/rfidreader/acl" exists
+	 * Create the file and directories if it doesn't exist
+	 * (user is whoever is running this)
+	 */
+	
 	public static void init() throws IOException{
 		//win
-		aclFile = new File("C:\\nonsync\\dev\\acl").getAbsolutePath();
-		//nix
-//		aclFile = new File(System.getProperty("user.home"),"rfid/rfidreader/acl").getAbsolutePath();		
-//		System.out.println(System.getProperty("user.home"));
+//		aclFile = new File("C:\\dev\\acl").getAbsolutePath();
 		
-		if(!new File(aclFile).exists()){throw new IOException("ACL file doesn't exist at: " + new File(aclFile).getAbsolutePath());}
+		//nix
+		aclFile = new File(System.getProperty("user.home"),"rfid/rfidreader/acl").getAbsolutePath();		
+		System.out.println(System.getProperty("user.home"));
+		
+		if(!new File(aclFile).exists()){
+//			throw new IOException("ACL file doesn't exist at: " + new File(aclFile).getAbsolutePath());
+		FileUtils.forceMkdir(new File(aclFile));
+		}
 //		System.out.println("ACL file is located at: " + aclFile);
 		
 	}
 	
-	public static Properties getHostProperties() throws IOException{
-		Properties properties = new Properties();
-		properties.load(HostProperties.class.getClassLoader().getResourceAsStream("config.properties"));
-		return properties;
-	}
+//	public static Properties getHostProperties() throws IOException{
+//		Properties properties = new Properties();
+//		properties.load(HostProperties.class.getClassLoader().getResourceAsStream("config.properties"));
+//		return properties;
+//	}
 	
 }
